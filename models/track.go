2243f39499a1b1fe842b0ae3ab515af3202c34d7
Gabriel Flynn <gm.flynn99@yahoo.com>
2021-03-30 19:42:56 -0500

    Updated json field names for Track model

--- models/track.go
+++ models/track.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type Track struct {
 	gorm.Model
-	Name      string
+	Name      string `json:"name"`
 	Address   string `gorm:"unique"`
-	URL       string
-	Latitude  float64
-	Longitude float64
+	URL       string  `json:"url"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64  `json:"longitude"`
 }
